a7ca39768e2957c547be0c6a8bb460e605b143c0
g g <gabedotgutierrez@gmail.com>
2024-10-01 15:09:40 -0400

    delete made, add broken

--- controllers/applications.js
+++ controllers/applications.js
@@ -6,13 +6,22 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // we will build out our router logic here
-router.get('/', (req,res)=>{
-    try{
-        res.render('applications/index.ejs')
-    } catch {
-        console.error()
+
+router.get('/', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Render index.ejs, passing in all of the current user's 
+      // applications as data in the context object. 
+      res.render('applications/index.ejs', {
+        applications: currentUser.applications,
+      });
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error)
+      res.redirect('/')
     }
-})
+  });
 
 router.post('/', async (req, res) => {
     try {
@@ -31,6 +40,46 @@ router.post('/', async (req, res) => {
       res.redirect('/')
     }
   });
+
+  // controllers/applications.js SHOW ROUTE
+
+  router.get('/:applicationId', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Find the application by the applicationId supplied from req.params
+      const application = currentUser.applications.id(req.params.applicationId);
+      // Render the show view, passing the application data in the context object
+      res.render('applications/show.ejs', {
+        application: application,
+      });
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/')
+    }
+  });
+
+  // controllers/applications.js DELETE ROUTE
+
+router.delete('/:applicationId', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Use the Mongoose .deleteOne() method to delete 
+      // an application using the id supplied from req.params
+      currentUser.applications.id(req.params.applicationId).deleteOne();
+      // Save changes to the user
+      await currentUser.save();
+      // Redirect back to the applications index view
+      res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/')
+    }
+  });
+  
 
 // controllers/applications.js
 
